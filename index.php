164fd45ccf793cb3c7ac1b79a91aacd71738cc08
mosmpy <mos92372901@gmail.com>
2014-11-19 00:22:20 +0700

    w1
    version 1.0

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<script type="text/javascript">
+function MM_jumpMenu(targ,selObj,restore){ //v3.0
+  eval(targ+".location='"+selObj.options[selObj.selectedIndex].value+"'");
+  if (restore) selObj.selectedIndex=0;
+}
+</script>
+</head>
+
+<body>
+<form id="form1" name="form1" method="post" action="">
+  <h1><em><strong>ร้านก๋วยเตี๋ยวต้มยำ บ้านลายจุดข้างวัง</strong></em></h1>
+  <p>ก๋วยเตี๋ยวต้มยำของร้านบ้านลายจุดข้างวัง เริ่มต้นในราคาชามละ 35 บาท</p>
+  <p>โดยทางร้านจะใส่ หมู ลูกชิ้น หมูสับ ตับหลวก แต่ถ้าลูกค้าท่านใดที่จะเลือกไม่รับของที่ทางร้านได้ใส่ให้ กรุณากดเลือกได้เลยค่ะ</p>
+  <p>
+    <input type="checkbox" name="t1" id="t1" />
+    <label for="t1"></label>
+  หมู</p>
+  <p>
+    <input type="checkbox" name="t2" id="t2" />
+    <label for="t2"></label>
+  ลูกชิ้น</p>
+  <p>
+    <label for="checkbox2"></label>
+    <input type="checkbox" name="t3" id="t3" />
+    <label for="t3"></label>
+  หมูสับ</p>
+  <p>
+    <label for="checkbox2"></label>
+    <input type="checkbox" name="t4" id="t4" />
+    <label for="t4"></label>
+  ตับหลวก</p>
+  <p>** กรุณาเลือกเส้น</p>
+  <p>
+    <label for="s2"></label>
+    <select name="jumpMenu4" id="jumpMenu4" onchange="MM_jumpMenu('parent',this,0)">
+      <option>เส้นใหญ่</option>
+      <option>เส้นเล็ก</option>
+      <option>หมีขาว</option>
+      <option>หมีเหลือง</option>
+      <option>เกาเหล่าเนื้อหมู</option>
+    </select>
+  </p>
+  <p>
+    <label for="s4"></label>
+  </p>
+  <p>** กรุณาเลือกท็อปปิ้ง</p>
+  <p>
+    <input type="checkbox" name="a1" id="a1" />
+    <label for="a1"></label>
+  กุ้งแม่น้ำ ตัวละ 55-. เลือกจำนวน
+  <label for="n1"></label>
+  <select name="jumpMenu" id="jumpMenu" onchange="MM_jumpMenu('parent',this,0)">
+    <option>0</option>
+    <option>1</option>
+    <option>2</option>
+    <option>3</option>
+    <option>4</option>
+    <option>5</option>
+  </select>
+  </p>
+  <p>
+    <input type="checkbox" name="a2" id="a2" />
+    <label for="a2"></label>
+    <label for="a3"></label>
+  ปลาหมึก ตัวละ 55-. เลือกจำนวน
+  <select name="jumpMenu2" id="jumpMenu2" onchange="MM_jumpMenu('parent',this,0)">
+    <option>0</option>
+    <option>1</option>
+    <option>2</option>
+    <option>3</option>
+    <option>4</option>
+    <option>5</option>
+  </select>
+  </p>
+  <p>
+    <input type="checkbox" name="a3" id="a3" />
+    <label for="a3"></label>
+  ใส่ไข่มะตูม ฟองละ 15-. เลือกจำนวน
+  <select name="jumpMenu3" id="jumpMenu3" onchange="MM_jumpMenu('parent',this,0)">
+    <option>0</option>
+    <option>1</option>
+    <option>2</option>
+    <option>3</option>
+    <option>4</option>
+    <option>5</option>
+  </select>
+  </p>
+  <p>** กรุณาเลือกรสชาติ</p>
+  <p>
+    <select name="jumpMenu5" id="jumpMenu5" onchange="MM_jumpMenu('parent',this,0)">
+      <option>น้ำใส</option>
+      <option>น้ำตก</option>
+      <option>ต้มยำ</option>
+      <option>เย็นตาโฟ</option>
+    </select>
+  </p>
+  <p>** จำนวนที่สั่ง
+    <label for="ch1"></label>
+  </p>
+  <p>** ราคา
+    <label for="pi1"></label>
+  </p>
+  <p>
+    <label for="a4"></label>
+  </p>
+</form>
+</body>
+</html>
